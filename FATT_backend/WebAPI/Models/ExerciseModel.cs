0b5f95d991e17ed681a4944c895441c6127ac72e
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-09-23 12:10:55 +0200

    Added Exercise and workout models+controllers

---
+++ FATT_backend/WebAPI/Models/ExerciseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public class ExerciseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string MuscleGroup { get; set; }
+        public string Equipment { get; set; }
+        public string Image { get; set; }
+    }
+}
