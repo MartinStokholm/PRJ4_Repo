875a02d396144efeff745f091b2ec6a8b3345ad4
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-10-18 12:34:07 +0200

    Revert "working on exercise controller"
    This reverts commit db83d5d0b1456ab285970cf99332a49fce2114a5.

--- FATT_backend/WebAPI/Controllers/ExerciseController.cs
+++ FATT_backend/WebAPI/Controllers/ExerciseController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebAPI.Dto;
+using WebAPI.Dto.Exercise;
+using WebAPI.Models;
+using Mapster;
 
 namespace WebAPI.Controllers
-{
+{ 
     [Route("api/[controller]")]
     [ApiController]
     public class ExerciseController : ControllerBase
@@ -12,6 +16,31 @@ namespace WebAPI.Controllers
         public ExerciseController(DataContext context)
         {
             _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ExerciseWithName>> PostExercise(ExerciseCreate exerciseCreate)
+        {
+            var exerciseToAdd = exerciseCreate.Adapt<ExerciseModel>();
+            _context.ExerciseModels.Add(exerciseToAdd);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetExercise", new { id = exerciseToAdd.Id }, exercise);
+        }
+        
+        /// <summary>
+        /// Get all exercises with their name
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("WithNames")]
+        public async Task<ActionResult<ExerciseWithName>> GetExercisesWithNames()
+        {
+            var dbExerciseWithName = await _context.ExerciseModels
+                .Include(e => e.Name)
+                .ToListAsync();
+            
+            return Ok(dbExerciseWithName);
         }
     }
 }
