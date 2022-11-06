f5adf96146cf404557898fa706de81362dffef0a
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-06 15:58:15 +0100

    Fetch data for exercise
    Changed the way to fetch data, so we take the data from initial query of exercises. We still update in the background. It's a small improvement

--- FATT_frontend/FATT_NEXT/fatt/pages/workout/index.tsx
+++ FATT_frontend/FATT_NEXT/fatt/pages/workout/index.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { useQuery } from "react-query";
 import fecthWorkouts from "../../src/fetchers/workout";
-import {useWorkoutsData} from "../../src/hooks/useWorkoutsData";
-import {useExercisesData} from "../../src/hooks/useExercisesData";
+import { useWorkoutsData } from "../../src/hooks/useWorkoutsData";
+import { useExercisesData } from "../../src/hooks/useExercisesData";
 import styles from "../../styles/Workout.module.css";
 
-
-const onSuccess = ( ExerciseData) => {
-  const exerciseIds = useWorkoutsData.data?.map()
+const onSuccess = (WorkoutData, ExerciseData) => {
+  {
+    /* Maybe we only should show data if success*/
+  }
 
   console.log("Perform side effect after data fetching", ExerciseData);
 };
@@ -16,28 +17,10 @@ const onError = (error) => {
   console.log(`Perform side effect after encountered error\n ${error}`);
 };
 
-// function getIndex(WorkoutData){
-//   return arr.findIndex(id => WorkoutData.data.exercisesIds == id )
-// }
-
-function showExercise(WorkoutData, ExerciseData){
-  {console.log("I AM BLUE ExerciseData")}
-  {ExerciseData?.data.map((exercise) => (
-    <div key={WorkoutData.data.exercisesIds}>
-      {exercise.id} {console.log(exercise.id)}
-    </div>
-  ))}
-}
-
 export default function Workout() {
- 
-  const { isLoading, data: WorkoutData, isError, error, } = 
-    useWorkoutsData()
-   
+  const { isLoading, data: WorkoutData, isError, error } = useWorkoutsData();
 
-  const { data: ExerciseData } = 
-  useExercisesData (onSuccess, onError)
-
+  const { data: ExerciseData } = useExercisesData(onSuccess, onError);
 
   if (isLoading) {
     return <h2>Loading</h2>;
@@ -47,28 +30,25 @@ export default function Workout() {
     return <h2>{error.message}</h2>;
   }
 
-
-
-
-
   return (
     <div>
-      <h2>Workouts</h2>
+      <h2 className={styles.titel}>Workouts</h2>
 
-      {/* This maps out all workouts with there exercise as names */}
-      {WorkoutData.data?.map((workout) => (
-        <div className={styles.titel} key={workout.name}>
-          {workout.name}
-          { ExerciseData?.data.map((exercise ) => (
-            workout.exercisesIds.includes(exercise.id) ?
-              ( <div className={styles.text} key={exercise.id}>
-              {exercise.name}             
-           </div>) : null
-            
-          ))}
+      <div className={styles.container}>
+        {/* This maps out all workouts with there exercise as names */}
+        {WorkoutData.data?.map((workout) => (
+          <div className={styles.caption} key={workout.name}>
+            {workout.name}
+            {ExerciseData?.data.map((exercise) =>
+              workout.exercisesIds.includes(exercise.id) ? (
+                <div className={styles.description} key={exercise.id}>
+                  {exercise.name}
+                </div>
+              ) : null
+            )}
+          </div>
+        ))}
       </div>
-      ))}
-
     </div>
   );
 }
