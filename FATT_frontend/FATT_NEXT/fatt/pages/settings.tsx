f0daa8ca1dd4a53fe7b8c2106e59953c62428609
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-06 16:13:00 +0100

    update

--- FATT_frontend/FATT_NEXT/fatt/pages/settings.tsx
+++ FATT_frontend/FATT_NEXT/fatt/pages/settings.tsx
@@ -1,10 +1,5 @@
 import Image from "next/image";
-import { ReactQuery } from "../src/components/ReactQuery";
 
 export default function Settings() {
-  return (
-    <div>
-      <ReactQuery />
-    </div>
-  );
+  return <div></div>;
 }
