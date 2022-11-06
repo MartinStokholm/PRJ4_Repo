fda348c3310f979cdeb29832db12591aab1efbfa
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-06 16:55:02 +0100

    stuff

--- FATT_frontend/FATT_NEXT/fatt/src/components/ExerciseItem.jsx
+++ FATT_frontend/FATT_NEXT/fatt/src/components/ExerciseItem.jsx
@@ -1,29 +1,32 @@
-import styles from '../../styles/ExerciseList.module.css'
-import Image from 'next/image'
+import styles from '../../styles/Exercise.module.css'
 import Youtube from 'react-youtube';
 
 const ExerciseItem = ({exercise}) => {
   
   const videoUrl = exercise?.data?.videoPath;
   const videoId = videoUrl?.split('be/')[1];
+
   return (
 
-    <div className={styles.itemWrapper}>
       <div className={styles.item}>
         <h1> {exercise?.data.name}</h1>
         <h2>Category: {exercise?.data.category}</h2>
         <h3>Intenisty: {exercise?.data.intensity}</h3>
-        <img src={exercise?.data.picturePath}/>
-        <p>Equipment: {exercise?.data.equipment}</p>
-        <p>Repetities: {exercise?.data.repetitions}</p>
-        <p>Sets: {exercise?.data.sets}</p>
+
+        <img src={exercise?.data.picturePath} width="500px"/>
+        
+        <div className={styles.description}>
+          <p>Equipment: {exercise?.data.equipment}</p>
+          <p>Repetities: {exercise?.data.repetitions}</p>
+          <p>Sets: {exercise?.data.sets}</p>
+        </div>
+        
         <p>Video showing how to perform {exercise?.data.name}</p>
         <Youtube  
             videoId= {videoId} 
             className={styles.video} 
         />
       </div>
-    </div>
   )
 }
 
