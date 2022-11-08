1505620d7abf388d9f0dab8bdd00c3edadb59da4
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-08 09:55:32 +0100

    Update test.tsx
    This is a temporary files to test the mutation before the completely functionality is implemented

--- FATT_frontend/FATT_NEXT/fatt/pages/workout/test.tsx
+++ FATT_frontend/FATT_NEXT/fatt/pages/workout/test.tsx
@@ -7,17 +7,23 @@ import {
   useUpdateWorkoutData,
 } from "../../src/mutation/PutWorkout";
 import { useDeleteWorkoutData } from "../../src/mutation/DeleteWorkout";
+import {
+  addExercisesToWorkout,
+  useAddExercisesToWorkoutData,
+} from "../../src/mutation/PostWorkoutAddExerciseList";
 
 export default function TestPage() {
   const [id, setId] = useState();
   const [name, setName] = useState("");
   const [duration, setDuration] = useState("");
   const [exercisesIds, setExerciseIds] = useState("");
+  const [exercisesId, setExerciseId] = useState();
   // I Get server error if we don't give id, maybe it works on real server without
   //const id = "44";
   const { mutate: addWorkout } = useAddWorkoutData();
   const { mutate: updateWorkout } = useUpdateWorkoutData();
   const { mutate: deleteWorkout } = useDeleteWorkoutData();
+  const { mutate: addExercisesToWorkout } = useAddExercisesToWorkoutData();
 
   const handleAddWorkoutClick = () => {
     console.log({ name, duration, exercisesIds });
@@ -33,6 +39,13 @@ export default function TestPage() {
     updateWorkout(workout);
   };
 
+  const handleaddExercisesToWorkoutClick = () => {
+    console.log({ id, name, duration, exercisesIds });
+
+    const workout = { id, name, duration, exercisesIds };
+    addExercisesToWorkout(workout);
+  };
+
   const handleDeleteWorkoutClick = () => {
     console.log({ id, name, duration, exercisesIds });
 
@@ -42,25 +55,43 @@ export default function TestPage() {
 
   return (
     <>
-      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <input
+        type="text"
+        placeholder="workoutId"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
       <input
         type="text"
         value={name}
+        placeholder="name"
         onChange={(e) => setName(e.target.value)}
       />
       <input
         type="text"
         value={duration}
+        placeholder="duration"
         onChange={(e) => setDuration(e.target.value)}
       />
       <input
         type="text"
+        placeholder="exercisesIds"
         value={exercisesIds}
         onChange={(e) => setExerciseIds(e.target.value)}
+      />
+      <input
+        type="text"
+        value={exercisesId}
+        placeholder="exercisesId"
+        onChange={(e) => setExerciseId(e.target.value)}
       />
       <Button onClick={handleAddWorkoutClick} text={"Add"} />
       <Button onClick={handleUpdateWorkoutClick} text={"Update"} />
       <Button onClick={handleDeleteWorkoutClick} text={"Delete"} />
+      <Button
+        onClick={handleaddExercisesToWorkoutClick}
+        text={"Add Exercises To Workout"}
+      />
       <div></div>
     </>
   );
