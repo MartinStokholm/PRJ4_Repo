c5051e9bf6041799f5fd7124faf0ecf37fe7b8b8
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-10 13:41:08 +0100

    removed wrong prop type from account

--- FATT_backend/WebAPI/Models/Account.cs
+++ FATT_backend/WebAPI/Models/Account.cs
@@ -10,7 +10,6 @@ namespace WebAPI.Models
         public double Weigth { get; set; }
         public string Gender { get; set; } = "";
         public int Age { get; set; }
-        public string PasswordHash { get; set; } = "";
         public string Email { get; set; } = "";
         public byte[] PasswordHash { get; set; } 
 
