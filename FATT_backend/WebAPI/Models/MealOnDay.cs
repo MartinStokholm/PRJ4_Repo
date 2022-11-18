1be7e62320311b0acd20b6cb4f436270bd981ac7
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-18 11:02:02 +0100

    Calender also has list of MealOnDay

--- FATT_backend/WebAPI/Models/MealOnDay.cs
+++ FATT_backend/WebAPI/Models/MealOnDay.cs
@@ -1,10 +1,10 @@
 ﻿namespace WebAPI.Models
 {
-    public class WorkoutOnDay
+    public class MealOnDay
     {
         public long Id { get; set; }
         
-        public long WorkoutId { get; set; }
+        public long MealId { get; set; }
 
         public string? Day { get; set; }
         
