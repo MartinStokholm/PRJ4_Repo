54e8f8ae9e80b9e4a4173baba63247d4e161e345
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-20 20:12:05 +0100

    styling on exercise create

--- FATT_frontend/FATT_NEXT/fatt/src/components/Exercise/ExerciseList.jsx
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Exercise/ExerciseList.jsx
@@ -4,53 +4,65 @@ import { TextInput } from "flowbite-react";
 import InputButton from "../Button/InputButton";
 const ExerciseList = ({ data }) => {
   return (
-    <div>
-      <div className="border rounded mb-4">
-        <Select
-          getOptionLabel={(option) => `${option.category}`}
-          options={data?.data?.category}
-          instanceId="category"
-          isMulti
-          placeholder="Filter by category"
-        />
-        <form clasName="flex">
+    <>
+      <div>
+        <div className="border rounded p-4 mb-4">
+          <Select
+            getOptionLabel={(option) => `${option.category}`}
+            options={data?.data?.category}
+            instanceId="category"
+            isMulti
+            placeholder="Filter by category"
+          />
+        </div>
+
+        <h1 className="mb-2 font-bold">Create an exercise</h1>
+        <form className="flex flex-wrap border rounded bg-grey-200">
           <TextInput
             type="text"
-            placeholder="newExerciseName"
+            className="mx-auto my-4 "
+            placeholder="Name"
             onChange={(e) => setWorkoutName(e.target.value)}
           />
           <TextInput
             type="text"
+            className="mx-auto my-4"
             placeholder="Category"
             onChange={(e) => setDurantion(e.target.value)}
           />
           <TextInput
             type="text"
+            className="mx-auto my-4"
             placeholder="Intensity"
             onChange={(e) => setDurantion(e.target.value)}
           />
           <TextInput
             type="text"
+            className="mx-auto my-4"
             placeholder="Equipment"
             onChange={(e) => setDurantion(e.target.value)}
           />
           <TextInput
             type="text"
+            className="mx-auto my-4"
             placeholder="Repetitions"
             onChange={(e) => setDurantion(e.target.value)}
           />
           <TextInput
             type="text"
+            className="mx-auto my-4"
             placeholder="Sets"
             onChange={(e) => setDurantion(e.target.value)}
           />
           <TextInput
             type="text"
+            className="mx-auto my-4"
             placeholder="PicturePath"
             onChange={(e) => setDurantion(e.target.value)}
           />
           <TextInput
             type="text"
+            className="mx-auto my-4"
             placeholder="VideoPath"
             onChange={(e) => setDurantion(e.target.value)}
           />
@@ -67,7 +79,7 @@ const ExerciseList = ({ data }) => {
           );
         })}
       </div>
-    </div>
+    </>
   );
 };
 
