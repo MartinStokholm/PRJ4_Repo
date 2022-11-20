1ebf0db3c3fc343ae3bf9b3b2646e38ff415dbb3
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-20 11:31:00 +0100

    Update PostRegister.tsx

--- FATT_frontend/FATT_NEXT/fatt/src/mutation/account/PostRegister.tsx
+++ FATT_frontend/FATT_NEXT/fatt/src/mutation/account/PostRegister.tsx
@@ -2,8 +2,6 @@ import { useMutation, useQueryClient } from "react-query";
 import axios, { AxiosResponse } from "axios";
 import { request } from "../../utils/axios";
 import { toast } from "react-toastify";
-import { middleware } from "../../components/Redirect";
-import { NextRequest } from "next/server";
 import { useRouter } from "next/router";
 
 import type { AccountNoIdDto } from "../../../interfaces/Account";
@@ -11,7 +9,12 @@ import { NextResponse } from "next/server";
 import { server } from "../../../config/config";
 
 export const postRegister = async (account: AccountNoIdDto) => {
-  return request({ url: `account/register`, method: "post", data: account });
+  const response = await axios({
+    url: `${server}account/register`,
+    method: "post",
+    data: account,
+  });
+  //return request({ url: `account/register`, method: "post", data: account });
 };
 
 export const usePostRegister = () => {
@@ -21,7 +24,6 @@ export const usePostRegister = () => {
     onSuccess: (newAccount) => {
       toast.success(`Account Created "${newAccount.data.name}"`);
       router.push("/login");
-      // () => middleware();
     },
     onError: (_error, _account, context) => {
       toast.error("Creating Account Failed");
