2cf94cb284a2883004aea2da8253672910e41d9a
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-20 12:15:44 +0100

    ok2

--- FATT_backend/WebAPI/Dto/Account/AccountDto.cs
+++ FATT_backend/WebAPI/Dto/Account/AccountDto.cs
@@ -3,9 +3,7 @@ namespace WebAPI.Dto.Account
     public class AccountDto
     {
         public string Name { get; set; } = string.Empty;
-        
-        public float Weigth { get; set; }
-        public int Age { get; set; }
+
         public string Email { get; set; } = string.Empty;
         
         public string Password {get; set; } = string.Empty;
