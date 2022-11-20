ada19b634945612f07dcd076c60b484fb43107e0
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-20 16:26:33 +0100

    styling

--- FATT_frontend/FATT_NEXT/fatt/src/components/Workout/WorkoutItem.jsx
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Workout/WorkoutItem.jsx
@@ -16,43 +16,49 @@ const WorkoutItem = ({ workoutData, exerciseData }) => {
 
   return (
     <>
-      <div>
-        <h1 className="mt-4 font-bold">{workoutData?.data?.name}</h1>
-        <h2 className="italic">{workoutData?.data?.duration}</h2>
+      <div className="flex">
+        <div>
+          <h1 className="mt-4 font-bold">{workoutData?.data?.name}</h1>
+          <h2 className="italic">{workoutData?.data?.duration}</h2>
 
-        {exerciseData?.data.map((exercise) =>
-          workoutData?.data?.exercisesIds?.includes(exercise.id) ? (
-            <div
-              key={exercise.id}
-              className="bg-white overflow-hidden shadow-lg mx-4 my-4"
-            >
-              <WorkoutItemThumbnail exercise={exercise} />
-              <MyButton
-                text={"Remove"}
-                onClick={() => {
-                  handleRemoveButtonClick(exercise.id);
-                }}
-              />
-            </div>
-          ) : null
-        )}
+          {exerciseData?.data.map((exercise) =>
+            workoutData?.data?.exercisesIds?.includes(exercise.id) ? (
+              <div
+                key={exercise.id}
+                className="bg-white overflow-hidden shadow-lg mx-4 my-4"
+              >
+                <WorkoutItemThumbnail exercise={exercise} />
+                <MyButton
+                  text={"Remove"}
+                  onClick={() => {
+                    handleRemoveButtonClick(exercise.id);
+                  }}
+                />
+              </div>
+            ) : null
+          )}
+        </div>
+        <div>
+          <h1 className="mt-4 font-bold">Exercise list</h1>
+          <h2 className="italic">Add exercise to workout</h2>
 
-        {exerciseData?.data.map((exercise) =>
-          workoutData?.data?.exercisesIds?.includes(exercise.id) ? null : (
-            <div
-              key={exercise.id}
-              className="bg-white overflow-hidden shadow-lg mx-4 my-4"
-            >
-              <WorkoutItemThumbnail exercise={exercise} />
-              <MyButton
-                text={"Add"}
-                onClick={() => {
-                  handleRemoveButtonClick(exercise.id);
-                }}
-              />
-            </div>
-          )
-        )}
+          {exerciseData?.data.map((exercise) =>
+            workoutData?.data?.exercisesIds?.includes(exercise.id) ? null : (
+              <div
+                key={exercise.id}
+                className="bg-white overflow-hidden shadow-lg mx-4 my-4"
+              >
+                <WorkoutItemThumbnail exercise={exercise} />
+                <MyButton
+                  text={"Add"}
+                  onClick={() => {
+                    handleRemoveButtonClick(exercise.id);
+                  }}
+                />
+              </div>
+            )
+          )}
+        </div>
       </div>
     </>
   );
