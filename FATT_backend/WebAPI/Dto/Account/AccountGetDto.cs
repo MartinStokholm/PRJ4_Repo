25a342c31a8bd1901e9f35d66e583ebcdd32db93
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-22 17:37:01 +0100

    AccountGet returns profile info

--- FATT_backend/WebAPI/Dto/Account/AccountGetDto.cs
+++ FATT_backend/WebAPI/Dto/Account/AccountGetDto.cs
@@ -3,5 +3,9 @@ namespace WebAPI.Dto.Account;
 public class AccountGetDto
 {
     public string Email {get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public double Weigth { get; set; }
+    public string? Gender { get; set; }
+    public int Age { get; set; }
 
 }
