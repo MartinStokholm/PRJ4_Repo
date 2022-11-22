c9cdadaa202f6b705898b41b215fe7b225de9939
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-22 12:31:04 +0100

    Update Nav.jsx

--- FATT_frontend/FATT_NEXT/fatt/src/components/Layout/Nav.jsx
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Layout/Nav.jsx
@@ -3,14 +3,12 @@ import { Navbar } from "flowbite-react";
 
 const Nav = () => {
   const [session, setSession] = useState(false);
-  // if (localStorage.getItem("token")) {
-  //   setSession(true);
-  // }
-  // if (typeof window !== "undefined") {
-  //   if (localStorage.getItem("token")) {
-  //     setSession(true);
-  //   }
-  // }
+
+  if (typeof window !== "undefined" && session == false) {
+    if (localStorage.getItem("token")) {
+      setSession(true);
+    }
+  }
 
   return (
     <Navbar
@@ -62,22 +60,21 @@ const Nav = () => {
         >
           Dishes
         </Navbar.Link>
-        {/* { setSession == null ? (   
-           <Navbar.Link
-           className="hover:text-green-500 md:hover:text-green-500"
-           href="/signout"
-         >
-           Logout
-         </Navbar.Link>
+        {setSession == null ? (
+          <Navbar.Link
+            className="hover:text-green-500 md:hover:text-green-500"
+            href="/signout"
+          >
+            Logout
+          </Navbar.Link>
         ) : (
-           <Navbar.Link
-          className="hover:text-green-500 md:hover:text-green-500"
-          href="/login"
-
-        >
-          Login
-        </Navbar.Link>) 
-        } */}
+          <Navbar.Link
+            className="hover:text-green-500 md:hover:text-green-500"
+            href="/login"
+          >
+            Login
+          </Navbar.Link>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
