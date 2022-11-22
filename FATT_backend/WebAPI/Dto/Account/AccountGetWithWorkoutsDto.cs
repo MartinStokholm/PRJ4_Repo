8a63e6771162a41af35738785b0358012a1e8528
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-22 17:15:53 +0100

    Added Dto for account get workouts

---
+++ FATT_backend/WebAPI/Dto/Account/AccountGetWithWorkoutsDto.cs
@@ -0,0 +1,10 @@
+﻿using WebAPI.Dto.Workout;
+
+namespace WebAPI.Dto.Account
+{
+    public class AccountGetWithWorkoutsDto
+    {
+        public string AccountEmail { get; set; }
+        public List<WorkoutWithExerciseFullDto> Workouts { get; set; } = new List<WorkoutWithExerciseFullDto>();
+    }
+}
