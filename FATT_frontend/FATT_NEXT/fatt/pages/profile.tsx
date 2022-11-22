5fac1227921a6a381e93be3733bee3ad4777c72c
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-22 12:39:39 +0100

    Update profile.tsx

--- FATT_frontend/FATT_NEXT/fatt/pages/profile.tsx
+++ FATT_frontend/FATT_NEXT/fatt/pages/profile.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Button from "../src/components/Button/Button";
 import Modal from "../src/components/Setting/Modal";
-import ModalAge from "../src/components/Setting/ModalAge";
-import ModalWeight from "../src/components/Setting/ModalWeigth";
+import ModalAge from "../src/components/Account/ModalAge";
+import ModalWeight from "../src/components/Account/ModalWeigth";
 /* This pages contain to separate comlumes like one for 
    settings and one for the profile picture
    We both show the information about the settings 
