2538b31b1469537a23998278bf10d49c52bf9dc1
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-22 12:34:47 +0100

    Revert "Added foreign key to Account for meals and workouts"
    This reverts commit 0fcc2d99b84326413b60dc6bc16c4ebfe23da89f.

--- FATT_backend/WebAPI/Models/Meal.cs
+++ FATT_backend/WebAPI/Models/Meal.cs
@@ -8,7 +8,5 @@ namespace WebAPI.Models
         public string? Name { get; set; }
 
         public List<Dish> Dishes { get; set; } = new List<Dish>();
-        public long AccountId { get; set; }
-        public Account Account { get; set; } = new Account();
     }
 }
--- FATT_backend/WebAPI/Models/Workout.cs
+++ FATT_backend/WebAPI/Models/Workout.cs
@@ -13,8 +13,5 @@ namespace WebAPI.Models
         public string Name { get; set; } = "";
         public string Duration { get; set; } = "";
         public virtual ICollection<Exercise> Exercises { get; set; } = new List<Exercise>();
-
-        public long AccountId { get; set; }
-        public Account Account { get; set; } = new Account();
     }
 }
