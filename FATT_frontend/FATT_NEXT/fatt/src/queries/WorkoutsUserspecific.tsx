bf17715abc81562c517090d1dbbd564a8c1f87f3
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-22 19:48:46 +0100

    React get user specific workouts

---
+++ FATT_frontend/FATT_NEXT/fatt/src/queries/WorkoutsUserspecific.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query";
+import { request } from "../utils/axios";
+
+const fecthWorkouts = async () => {
+  const response = await request({
+    url: `workout/account${localStorage.getItem("email")}`,
+    method: "get",
+  });
+  console.log(response.status);
+  if (response.status == 304) {
+    throw new Error("Problem fetching data");
+  }
+  const exercise = await response.data;
+  //assertIsExercise(exercise);
+
+  return response;
+};
+
+export const getWorkoutsList = (onSuccess, onError) => {
+  return useQuery("workoutsKey", fecthWorkouts, {
+    refetchOnWindowFocus: true,
+    onSuccess,
+    onError,
+  });
+};
+
+export default getWorkoutsList;
