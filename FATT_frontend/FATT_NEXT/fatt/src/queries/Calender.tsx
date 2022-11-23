1b46208e1e0bf096bf8dfd21d980699b62a3de55
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-23 08:18:02 +0100

    Update Calender.tsx

--- FATT_frontend/FATT_NEXT/fatt/src/queries/Calender.tsx
+++ FATT_frontend/FATT_NEXT/fatt/src/queries/Calender.tsx
@@ -3,9 +3,12 @@ import { request } from "../utils/axios";
 
 export const fetchCalender = async ({ queryKey }) => {
   const id = queryKey[1];
-  return request({ url: `Calenders/${id}`, method: "get" });
+  return request({
+    url: `account/${localStorage.getItem("email")}/calenders/`,
+    method: "get",
+  });
 };
 
 export const GetCalender = (id) => {
-  return useQuery([`calendersKey`, id], fetchCalender, {});
+  return useQuery([`calenderKey`], fetchCalender, {});
 };
