52dcfb3adb148f7a45c68c0f548cffa531b5abfa
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-23 09:07:45 +0100

    styling on login

--- FATT_frontend/FATT_NEXT/fatt/pages/login.tsx
+++ FATT_frontend/FATT_NEXT/fatt/pages/login.tsx
@@ -6,12 +6,6 @@ export default function login() {
   return (
     <>
       <Login />
-      {/* <Link
-        href="/signUp"
-        className="mx-4 text-center hover:underline hover:font-semibold hover:text-green-500 md:hover:text-green-500"
-      >
-        Click here to sign up
-      </Link> */}
     </>
   );
 }
--- FATT_frontend/FATT_NEXT/fatt/src/components/Account/Login.tsx
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Account/Login.tsx
@@ -54,13 +54,6 @@ const Login = (props) => {
       <Modal IsVisible={showModal} onClose={() => setShowModal(false)}>
         <SignUp />
       </Modal>
-
-      <Link
-        href="/signUp"
-        className="mx-4 text-center hover:underline hover:font-semibold hover:text-green-500 md:hover:text-green-500"
-      >
-        Click here to sign up
-      </Link>
     </div>
   );
 };
