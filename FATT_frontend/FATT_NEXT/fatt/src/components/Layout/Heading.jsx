94816ba582116d1b43ade14794faf82f04778657
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-24 23:19:28 +0100

    Headings component added

---
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Layout/Heading.jsx
@@ -0,0 +1,5 @@
+const Heading = ({ text }) => {
+  return <h1 className="font-extrabold m-4">{text}</h1>;
+};
+
+export default Heading;
---
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Layout/SubHeading.jsx
@@ -0,0 +1,5 @@
+const SubHeading = ({ text }) => {
+  return <h2 className="font-bold m-2">{text}</h2>;
+};
+
+export default SubHeading;
