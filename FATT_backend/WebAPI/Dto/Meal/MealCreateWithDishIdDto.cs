c620e3d2edc8e89d86ebd9ac387149099755eed8
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-25 10:13:53 +0100

    Added endpoint for return dishesId

---
+++ FATT_backend/WebAPI/Dto/Meal/MealCreateWithDishIdDto.cs
@@ -0,0 +1,11 @@
+﻿using WebAPI.Dto.Dish;
+using WebAPI.Models;
+
+namespace WebAPI.Dto.Meal
+{
+    public class MealCreateWithDishesIdsDto
+    {
+        public string Name { get; set; } = "";
+        public List<long> DishesIds { get; set; } = new List<long>();
+    }
+}
