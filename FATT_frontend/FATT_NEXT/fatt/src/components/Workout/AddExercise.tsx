3458a0f329553a72a31012d9c9aeb52d8d3d6786
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-25 10:44:18 +0100

    React refactored workoutItem into to other files AddExercise and RemoveExercise

---
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Workout/AddExercise.tsx
@@ -0,0 +1,44 @@
+import Button from "../Button/Button";
+import WorkoutItemThumbnail from "./WorkoutItemThumbnail";
+import { useRouter } from "next/router";
+import { useUpdateWorkoutAddExerciseData } from "../../mutation/workout/PutWorkoutAddExercise";
+
+const AddExercise = ({ workoutData, exerciseData }) => {
+  const workoutId = useRouter().query.id;
+
+  const handleAddButtonClick = (exerciseId) => {
+    updateWorkoutAddExercise({ workoutId, exerciseId });
+  };
+
+  const { mutate: updateWorkoutAddExercise } =
+    useUpdateWorkoutAddExerciseData();
+
+  return (
+    <div>
+      <h1 className="mt-4 font-bold">All exercises</h1>
+      <h2 className="italic">
+        That you can add to the workout: {workoutData?.data?.name}
+      </h2>
+
+      {exerciseData?.data.map((exercise) =>
+        workoutData?.data?.exercisesIds?.includes(exercise.id) ? null : (
+          <div
+            key={exercise.id}
+            className="bg-white overflow-hidden shadow-lg mx-4 my-4 flex"
+          >
+            <Button
+              text={"Add"}
+              onClick={() => {
+                handleAddButtonClick(exercise.id);
+              }}
+              key={undefined}
+            />
+            <WorkoutItemThumbnail exercise={exercise} />
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default AddExercise;
