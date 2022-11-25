123223cb5f23b4cdc8ba5b28b659738599cc486b
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-11-25 17:05:26 +0100

    Missing on refactor meal

--- FATT_frontend/FATT_NEXT/fatt/src/components/Meal/MealList.tsx
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Meal/MealList.tsx
@@ -9,8 +9,9 @@ import ModalDropdown from "../util/ModalDropdown";
 import { useDeleteMeal } from "../../mutation/meal/DeleteMeal";
 import AddMealToCalendarModal from "./AddMealToCalendarModal";
 import Heading from "../Layout/Heading";
+import MealHeader from "./MealHeader";
 
-const MealList = ({ mealData, dishData }) => {
+const MealList = ({ mealData }) => {
   const [showModal, setShowModal] = useState(false);
   const [showAddToCalendarModal, setShowAddToCalendarModal] = useState(false);
   const [idPassToModal, setIdPassToModal] = useState();
@@ -25,11 +26,7 @@ const MealList = ({ mealData, dishData }) => {
       <Heading text="My Personal Meals" />
       <div className="border rounded border-grey-300 bg-white overflow-hidden shadow-lg flex flex-col p-4">
         <div className="text-center">
-          <Button
-            onClick={() => setShowModal(true)}
-            text={"Create Meal"}
-            key={undefined}
-          />
+          <Button onClick={() => setShowModal(true)} text={"Create Meal"} />
         </div>
 
         <Modal IsVisible={showModal} onClose={() => setShowModal(false)}>
@@ -40,7 +37,10 @@ const MealList = ({ mealData, dishData }) => {
           IsVisible={showAddToCalendarModal}
           onClose={() => setShowAddToCalendarModal(false)}
         >
-          <AddMealToCalendarModal id={idPassToModal} />
+          <AddMealToCalendarModal
+            id={idPassToModal}
+            onClose={() => setShowModal(false)}
+          />
         </ModalDropdown>
 
         <div className="flex flex-wrap justify-center">
@@ -50,33 +50,8 @@ const MealList = ({ mealData, dishData }) => {
               key={meal.name}
               className="rounded bg-white shadow-lg w-full md:w-1/3 m-4 flex flex-col justify-center"
             >
-              <div className="flex flex-col justify-center">
-                <Link
-                  href={{ pathname: `/meal/${meal.id}` }}
-                  key={meal.id}
-                  className="hover:bg-green-50 hover:shadow-inner flex justify-center"
-                >
-                  <h1 className="m-2 font-bold py-1">{meal.name}</h1>
-                </Link>
-              </div>
-              <div>
-                {dishData?.data.map((dish) =>
-                  meal?.dishesIds?.includes(dish.id) ? (
-                    <div
-                      key={dish.id}
-                      className="bg-white overflow-hidden shadow-lg mx-4 my-4"
-                    >
-                      <MealItemThumbnail dish={dish} />
-                    </div>
-                  ) : null
-                )}
-              </div>
-              <DeleteButton
-                text={"Delete"}
-                onClick={() => {
-                  handleDeleteButtonClick(meal.id);
-                }}
-              />
+              <MealHeader meal={meal} />
+
               <Button
                 text={"Add To Calendar"}
                 onClick={() => {
@@ -84,6 +59,13 @@ const MealList = ({ mealData, dishData }) => {
                   setIdPassToModal(meal.id);
                 }}
                 key={undefined}
+              />
+
+              <DeleteButton
+                text={"Delete"}
+                onClick={() => {
+                  handleDeleteButtonClick(meal.id);
+                }}
               />
             </div>
           ))}
