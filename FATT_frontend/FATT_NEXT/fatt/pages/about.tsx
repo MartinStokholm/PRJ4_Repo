f843f5635804754f1f1bb785c916ccfad89704bc
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-25 01:17:10 +0100

    hmm

--- FATT_frontend/FATT_NEXT/fatt/pages/about.tsx
+++ FATT_frontend/FATT_NEXT/fatt/pages/about.tsx
@@ -1,8 +1,12 @@
 export default function AboutPage() {
   return (
     <>
-      <div className="bg-red-600 m-8">
-        <h1>About page for descibtion of the website</h1>
+      <div className="border rounded border-grey-300 bg-white overflow-hidden shadow-lg flex flex-col p-4">
+        <h1>About page for desciption of the website</h1>
+        <h2>
+          or maybe add a funny joke since who really ever goes to the about
+          page?
+        </h2>
       </div>
     </>
   );
