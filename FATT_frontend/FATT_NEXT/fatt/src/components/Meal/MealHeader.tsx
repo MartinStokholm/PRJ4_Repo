85c10ec908053af3e24686fd10f8e32f892b36cc
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-11-27 16:53:16 +0100

    Added dish count to meal

--- FATT_frontend/FATT_NEXT/fatt/src/components/Meal/MealHeader.tsx
+++ FATT_frontend/FATT_NEXT/fatt/src/components/Meal/MealHeader.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 
 const MealItem = ({ meal }) => {
+  var dishesCount = 0;
+  if (meal.dishes) {
+    dishesCount = meal.dishes.length;
+  }
   return (
     <div className="flex flex-col justify-center">
       <Link
@@ -9,6 +13,8 @@ const MealItem = ({ meal }) => {
         className="hover:bg-green-50 hover:shadow-inner flex justify-center"
       >
         <h1 className="m-2 font-bold py-1">{meal.name}</h1>
+
+        <h2 className="m-2 italic py-1">{dishesCount} dishes</h2>
       </Link>
     </div>
   );
